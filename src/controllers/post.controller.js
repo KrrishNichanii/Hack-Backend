02b26ca42192182ba6a41162220519629dea7455
Daksh-knows <dakshjain624@gmail.com>
2025-02-09 00:05:31 +0530

    Added Socket API for new Post creation

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -2,26 +2,29 @@ import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-
 //tested
 const createPost = async (req, res) => {
   try {
-    const { title, content, tags, userId , criticality} = req.body;
+    const { title, content, tags, userId, criticality } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("User not found");
+    }
     if (!title || !content) {
       throw new Error("All fields are required");
     }
 
     // Check if media is provided
     if (!req.file) {
-      throw new Error('Media is required');
+      throw new Error("Media is required");
     }
 
     // Upload media to Cloudinary
     const mediaLocalPath = req?.file?.path;
     const media = await uploadOnCloudinary(mediaLocalPath);
-    
+
     if (!media) {
-      throw new Error('Unable to upload media');
+      throw new Error("Unable to upload media");
     }
 
     // Create the post with media
@@ -34,10 +37,11 @@ const createPost = async (req, res) => {
       },
       tags: tags,
       user: userId,
-      comments: [] , 
-      criticality , 
-      verifiedBy: [] , 
-      disapprovedBy: [] , 
+      comments: [],
+      criticality,
+      verifiedBy: [],
+      disapprovedBy: [],
+      location: user.location,
     });
 
     // Fetch the newly created post and send response
@@ -48,180 +52,219 @@ const createPost = async (req, res) => {
     return res.status(200).send({
       success: true,
       message: "Successfully added post",
-      data: populatedPost
+      data: populatedPost,
     });
   } catch (error) {
     return res.status(400).send({
       success: false,
       message: error.message,
-      data: {}
+      data: {},
     });
   }
 };
 
 //tested
-const likePost = async  (req, res) => {
-    try { 
-        const { postId } = req.body ; 
-        console.log('Post id ',req.body);
-        
-        if(!postId){
-          throw new Error("Post ID is required") ;  
-        }
+const likePost = async (req, res) => {
+  try {
+    const { postId } = req.body;
+    console.log("Post id ", req.body);
 
-        // Like Post
-        let post = await Post.findById(postId) ; 
-        if(!post){
-          throw new Error("Post not found") ; 
-        }
-
-        post.likes += 1 ; 
-        await post.save() ;
-        post = await Post.findById(postId) ; 
-        res.status(200).send({success: true , message: "Post liked successfully" , data: post}) ; 
-    } catch (error) {
-        res.send({success: false , message: error.message , data: {}}) ; 
+    if (!postId) {
+      throw new Error("Post ID is required");
     }
-}
 
-//tested
-const deletePost = async (req, res)=>{
-    try {
-        const { postId } = req.body ; 
-        if(!postId){
-          throw new Error("Post ID is required") ; 
-        }
-        // Delete Post
-        const post = await Post.findByIdAndDelete(postId) ; 
-        res.send({success: true ,message: "Post deleted successfully" , data:post}) ; 
-    } catch (error) {
-        res.send({success: false , message: error.message , data: {}}) ; 
+    // Like Post
+    let post = await Post.findById(postId);
+    if (!post) {
+      throw new Error("Post not found");
     }
+
+    post.likes += 1;
+    await post.save();
+    post = await Post.findById(postId);
+    res
+      .status(200)
+      .send({ success: true, message: "Post liked successfully", data: post });
+  } catch (error) {
+    res.send({ success: false, message: error.message, data: {} });
+  }
 };
 
-
 //tested
-const getAllPosts = async(req , res) => {
+const deletePost = async (req, res) => {
   try {
-    const posts = await Post.find({}) ; 
-    return res.status(200).json({success: true , message: "Posts fetched successfully" , data: posts}) ; 
+    const { postId } = req.body;
+    if (!postId) {
+      throw new Error("Post ID is required");
+    }
+    // Delete Post
+    const post = await Post.findByIdAndDelete(postId);
+    res.send({
+      success: true,
+      message: "Post deleted successfully",
+      data: post,
+    });
   } catch (error) {
-    return res.status(400).json({success: false , message: error.message , data: {}}) ; 
+    res.send({ success: false, message: error.message, data: {} });
   }
-}
-
-
+};
 
 //tested
-const getPostsByUserId = async(req , res) => {
+const getAllPosts = async (req, res) => {
   try {
-    const { userId } = req.params ; 
+    const posts = await Post.find({});
+    return res.status(200).json({
+      success: true,
+      message: "Posts fetched successfully",
+      data: posts,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: error.message, data: {} });
+  }
+};
+
+//tested
+const getPostsByUserId = async (req, res) => {
+  try {
+    const { userId } = req.params;
     // console.log('UserId ' , req.query);
-    if(!userId) throw new Error("User ID is required") ; 
-    
-    const posts = await Post.find({user: userId}) ; 
-    return res.status(200).json({success: true , message: "Posts fetched successfully" , data: posts}) ; 
-  } catch (error) {
-    return res.status(400).json({success: false , message: error.message , data: {}}) ; 
-  }
-}
+    if (!userId) throw new Error("User ID is required");
 
+    const posts = await Post.find({ user: userId });
+    return res.status(200).json({
+      success: true,
+      message: "Posts fetched successfully",
+      data: posts,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: error.message, data: {} });
+  }
+};
 
 //tested
-const getPostById = async (req , res) => {
+const getPostById = async (req, res) => {
   try {
-    const { postId } = req.params ; 
+    const { postId } = req.params;
 
-    if(!postId) throw new Error("Post ID is required") ; 
-    
-    const post = await Post.findById(postId) ; 
-    return res.status(200).json({success: true , message: "Post fetched successfully" , data: post}) ; 
+    if (!postId) throw new Error("Post ID is required");
+
+    const post = await Post.findById(postId);
+    return res.status(200).json({
+      success: true,
+      message: "Post fetched successfully",
+      data: post,
+    });
   } catch (error) {
-    return res.status(400).json({success: false , message: error.message , data: {}}) ; 
+    return res
+      .status(400)
+      .json({ success: false, message: error.message, data: {} });
   }
-}
+};
 
-const addVerification = async (req , res) => {
-   const {severity , postId , userId} = req.body ; 
+const addVerification = async (req, res) => {
+  const { severity, postId, userId } = req.body;
 
-   try {
-     if(!userId) throw new Error("UserId is required") ; 
+  try {
+    if (!userId) throw new Error("UserId is required");
 
-     let user = await User.findById(userId) ; 
+    let user = await User.findById(userId);
 
-     if(!user) throw new Error("User not found") ; 
+    if (!user) throw new Error("User not found");
 
-     if(user.credit < 5) throw new Error("User is unauthorized to verify") ; 
-      
-     let post = await Post.findById(postId) ; 
-    
-     if(!post) throw new Error("Post not found") ; 
-      
-     if(post.status === 'Disapproved') throw new Error("Cannot verify disapproved posts") ; 
-     
-     if(post.verifiedBy.includes(userId)) throw new Error("Already verified by you") ; 
+    if (user.credit < 5) throw new Error("User is unauthorized to verify");
 
-     post.verifiedBy.push(userId) ; 
-     post.criticality = severity ; 
-     
-     if(post.verifiedBy.length === 3){
-       // TODO : send notification
-       //send postId and verifies user's severity
-       const users = await User.find({ _id: { $in: post.verifiedBy } }) ; 
-       for(let i=0;i<users.length;i++){
-         users[i].credit += 1 ; 
-         await users[i].save() ; 
-        }
+    let post = await Post.findById(postId);
 
-        post.status = "Verified"
-      }
-      await post.save() ; 
-     post = await Post.findById(post._id) ; 
-     res.status(200).json({success: true , message: "Successfully approved post" , data: post})
-   } catch (error) {
-       res.status(400).json({success: false , message: error.message , data: {}}) ; 
-   }
-}
+    if (!post) throw new Error("Post not found");
 
-const addDisapproval = async (req , res) => {
-   const { postId , userId} = req.body ; 
+    if (post.status === "Disapproved")
+      throw new Error("Cannot verify disapproved posts");
 
-   try {
-     if(!userId) throw new Error("UserId is required") ; 
-     let user = await User.findById(userId) ;
+    if (post.verifiedBy.includes(userId))
+      throw new Error("Already verified by you");
 
-     if(!user) throw new Error("User not found") ;
+    post.verifiedBy.push(userId);
+    post.criticality = severity;
 
-     if(user.credit < 5) throw new Error("User is unauthorized to disapprove") ;
-
-     let post = await Post.findById(postId) ; 
-     if(!post) throw new Error("Post not found") ; 
-     if(post.status === 'Verified') throw new Error("Cannot disapprove verified posts") ; 
-     
-     if(post.disapprovedBy.find(item => item === userId)) throw new Error("Already disapproved by you") ; 
-
-     post.disapprovedBy.push(userId) ; 
-    //  post.criticality = severity ; 
-    
-    if(post.disapprovedBy.length === 2){
+    if (post.verifiedBy.length === 3) {
       // TODO : send notification
       //send postId and verifies user's severity
-      const users = await User.find({ _id: { $in: post.verifiedBy } }) ; 
-      for(let i=0;i<users.length;i++){
-        users[i].credit -= 1 ; 
-        await users[i].save() ; 
+      const users = await User.find({ _id: { $in: post.verifiedBy } });
+      for (let i = 0; i < users.length; i++) {
+        users[i].credit += 1;
+        await users[i].save();
       }
 
-      post.status = "Disapproved"
+      post.status = "Verified";
     }
-     await post.save() ; 
-     post = await Post.findById(post._id) ; 
-     res.status(200).json({success: true , message: "Successfully disapproved post" , data: post})
-   } catch (error) {
-       res.status(400).json({success: false , message: error.message , data: {}}) ; 
-   }
-}
+    await post.save();
+    post = await Post.findById(post._id);
+    res.status(200).json({
+      success: true,
+      message: "Successfully approved post",
+      data: post,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message, data: {} });
+  }
+};
 
-export { createPost, deletePost, likePost , getAllPosts , getPostsByUserId , getPostById , addVerification , addDisapproval};
+const addDisapproval = async (req, res) => {
+  const { postId, userId } = req.body;
 
+  try {
+    if (!userId) throw new Error("UserId is required");
+    let user = await User.findById(userId);
 
+    if (!user) throw new Error("User not found");
+
+    if (user.credit < 5) throw new Error("User is unauthorized to disapprove");
+
+    let post = await Post.findById(postId);
+    if (!post) throw new Error("Post not found");
+    if (post.status === "Verified")
+      throw new Error("Cannot disapprove verified posts");
+
+    if (post.disapprovedBy.find((item) => item === userId))
+      throw new Error("Already disapproved by you");
+
+    post.disapprovedBy.push(userId);
+    //  post.criticality = severity ;
+
+    if (post.disapprovedBy.length === 2) {
+      // TODO : send notification
+      //send postId and verifies user's severity
+      const users = await User.find({ _id: { $in: post.verifiedBy } });
+      for (let i = 0; i < users.length; i++) {
+        users[i].credit -= 1;
+        await users[i].save();
+      }
+
+      post.status = "Disapproved";
+    }
+    await post.save();
+    post = await Post.findById(post._id);
+    res.status(200).json({
+      success: true,
+      message: "Successfully disapproved post",
+      data: post,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message, data: {} });
+  }
+};
+
+export {
+  createPost,
+  deletePost,
+  likePost,
+  getAllPosts,
+  getPostsByUserId,
+  getPostById,
+  addVerification,
+  addDisapproval,
+};
--- src/server.js
+++ src/server.js
@@ -11,18 +11,19 @@ import aiRouter from "./routes/askai.router.js";
 
 import http from "http";
 import { Server } from "socket.io";
-
+import Post from "./models/post.model.js";
+import User from "./models/user.model.js";
+import { getDistance } from "./utils/functions.js";
 
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server ,{
-    cors:{
-      origin: process.env.FRONTEND_URL || '*' , 
-      methods:['GET',"POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL || "*",
+    methods: ["GET", "POST"],
+  },
 });
-
 
 app.use(
   cors({
@@ -31,11 +32,41 @@ app.use(
   })
 );
 
+io.on("connection", (socket) => {
+  // console.log("Hello from ", socket.handshake.query);
+  socket.on("newPost", async ({ postId }) => {
+    console.log("post", postId);
 
-io.on("connection" , (socket) => {
-  console.log("Hello from ",socket);
-})
+    const post = await Post.findById(postId);
+    if (!post) {
+      throw new Error("Post not found");
+    }
+    if (post.criticality === "Severe" || post.status === "Verified") {
+      io.sockets.sockets.forEach(async (socketInstance) => {
+        const userId = socketInstance.handshake.query.userId;
+        const user = await User.findById(userId);
+        if (!user) {
+          throw new Error("User not found");
+        }
+        const distance = getDistance(post.location, user.location);
+        console.log("helo", distance);
 
+        if (
+          (post.criticality === "Severe" && distance <= 300) ||
+          (post.criticality === "Moderate" && distance <= 750) ||
+          (post.criticality === "Minor" && distance <= 2000)
+        ) {
+          socketInstance.emit("notification", post);
+        }
+      });
+    }
+  });
+  io.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  // console.log("User connected");
+});
 
 app.use(express.json({ limit: "32kb" }));
 app.use(express.urlencoded({ extended: true, limit: "32kb" }));
